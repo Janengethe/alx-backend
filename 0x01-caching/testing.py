99eee88c08cff5b17891ddcdadbef7b90b328873
Janengethe <janengethew@gmail.com>
2022-05-03 15:15:10 +0300

    task 1

---
+++ 0x01-caching/testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+diction = {"name": "an", 2: "ann", 3: "annn"}
+l = []
+key_s = diction.keys()
+print(key_s)
+l.append(key_s)
+print (l)
+# to_del = diction.pop(2)
+# print(to_del)
+# print(diction)
