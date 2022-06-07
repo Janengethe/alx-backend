2cb5657d820d371db61694b3a546380cf8ac1c60
Janengethe <janengethew@gmail.com>
2022-06-07 08:11:23 +0000

    10

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const obj = {
+  phoneNumber: '1234567890',
+  message: 'Verify account',
+};
+
+const job = queue.create('push_notification_code', obj).save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
