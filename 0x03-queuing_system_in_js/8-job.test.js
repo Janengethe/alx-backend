1ae970469eb3c2c48dba12cb7701358c68acd40b
Janengethe <janengethew@gmail.com>
2022-06-08 07:36:16 +0000

    all

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -5,23 +5,19 @@ const createPushNotificationsJobs = require('./8-job');
 const queue = kue.createQueue();
 
 describe('createPushNotificationsJobs', () => {
-  before(() => {
-    queue.testMode.enter(true);
+  before(() => queue.testMode.enter());
+  afterEach(() => queue.testMode.clear());
+  after(() => queue.testMode.exit());
+
+  it('displays an error message if jobs is not an array', () => {
+    const job = {
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account',
+    };
+    expect(() => createPushNotificationsJobs(job, queue)).to.throw(Error, 'Jobs is not an array');
   });
 
-  afterEach(() => {
-    queue.testMode.clear();
-  });
-
-  after(() => {
-    queue.testMode.exit();
-  });
-
-  it('display an error message if jobs is not an array', () => {
-    expect(() => createPushNotificationsJobs('job', queue)).to.throw(Error, 'Jobs is not an array');
-  });
-
-  it('test whether jobs are created', () => {
+  it('creates two new jobs to the queue', () => {
     const jobs = [
       {
         phoneNumber: '4153518780',
@@ -32,15 +28,19 @@ describe('createPushNotificationsJobs', () => {
         message: 'This is the code 4562 to verify your account',
       },
     ];
-
     createPushNotificationsJobs(jobs, queue);
-
     expect(queue.testMode.jobs.length).to.equal(2);
 
     expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
-    expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+    expect(queue.testMode.jobs[0].data).to.deep.equal({
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account',
+    });
 
     expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
-    expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+    expect(queue.testMode.jobs[1].data).to.deep.equal({
+      phoneNumber: '4153518781',
+      message: 'This is the code 4562 to verify your account',
+    });
   });
 });
